a370cc53ff433dd9e421853fb099a837331833c5
tjecarb <65381549+tjecarb@users.noreply.github.com>
2021-02-17 22:16:06 -0600

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -5,7 +5,7 @@ const descElement = document.querySelector('.temperature-description p');
 // App data
 const weather = {};
 weather.temperature = {
-  unit: 'celcius',
+  unit: 'celsius',
 };
 
 // Change to 'F' for Fahrenheit
