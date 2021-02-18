9047e20736b824c682949cd62edb63366658919a
tjecarb <65381549+tjecarb@users.noreply.github.com>
2021-02-17 22:06:50 -0600

    Update greeting.js

--- js/greeting.js
+++ js/greeting.js
@@ -3,7 +3,7 @@ const today = new Date();
 const hour = today.getHours();
 
 // Here you can change your name
-const name = ' Trevor';
+const name = 'Trevor';
 
 // Here you can change your greetings
 const gree1 = 'Go to Sleep!  ';
